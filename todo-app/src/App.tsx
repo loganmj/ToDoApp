9bc00b97609c28c1360fc138d44aa175c2d39daa
Michael <michaellogan7860@gmail.com>
2024-06-19 13:12:19 -0500

    Defined a data array for todo items.

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import TodoItem from "./components/TodoItem";
 
 const App: React.FC = () => {
+  // Define the data array
+  const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Sleep", completed: false },
+    { id: "todo-2", name: "Repeat", completed: false },
+  ];
+
   return (
     <ul
       role="list"
       className="todo-list stack-large stack-exception"
       aria-labelledby="list-heading"
     >
-      <TodoItem id="todo-0" name="Eat" completed />
-      <TodoItem id="todo-1" name="Sleep" />
-      <TodoItem id="todo-2" name="Repeat" />
+      {
+        // Iterate over data array to add TodoItems
+        DATA.map((task) => (
+          <TodoItem id={task.id} name={task.name} completed={task.completed} />
+        ))
+      }
     </ul>
   );
 };
